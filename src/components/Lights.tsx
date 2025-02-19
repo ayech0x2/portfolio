f4c05ca7135a7d7273a0534e836a354bc477df8d
Hamza Ayech <4yech.hamza@gmail.com>
2025-02-19 22:27:30 +0100

    Refactoring lights

--- src/components/Lights.tsx
+++ src/components/Lights.tsx
@@ -3,12 +3,12 @@ export default function Lights() {
     <group>
       <pointLight
         intensity={1}
-        position={[-0.733132004737854, 1, 0.5]} // Adjust position as needed
-        rotation={[-1.7138244961074722, 0, 0]}
+        position={[-0.9, 1, 0.5]}
+        rotation={[-1.7, 0, 0]}
       />
       <pointLight
         intensity={1}
-        position={[1.310068964958191, 0.909714937210083, 0.3185810148715973]} // Adjust position as needed
+        position={[1.3, 0.9, 0.3]}
         rotation={[-1.7138244961074722, 0, 0]}
       />
       <pointLight
@@ -20,21 +20,10 @@ export default function Lights() {
         intensity={1}
         position={[
           -0.5815072655677795, -0.008714079856872559, 1.1628460884094238,
-        ]} // Adjust position as needed
-        rotation={[0.3297934002502259, -0.3816346895677042, 2.3059210758727193]} // Rotate if necessary
+        ]}
+        rotation={[0.3297934002502259, -0.3816346895677042, 2.3059210758727193]}
       />
-      <pointLight
-        intensity={0.5}
-        position={[
-          2.2421510219573975, -0.008714079856872559, -1.1785615682601929,
-        ]} // Adjust position as needed
-        rotation={[-3.072920768296241, 0.8016682978737345, 1.601065067170026]} // Rotate if necessary
-      />
-      <pointLight
-        intensity={0.5}
-        position={[-1.622774362564087, 0, 0.12780606746673584]} // Adjust position as needed
-        rotation={[-1.5707964460041863, -1.5707963267948966, 0]} // Rotate if necessary
-      />
+
       <pointLight
         intensity={0.5}
         position={[-0.3715713948, 1, -0.27319374084]}
