eed2a63954bc2b98a51f47542a1324433e4cb880
Hamza Ayech <4yech.hamza@gmail.com>
2025-02-19 22:28:37 +0100

    Adding the hook thats resonsible for scene interactions

--- src/hooks/use-scene-interactions.ts
+++ src/hooks/use-scene-interactions.ts
@@ -1,20 +1,27 @@
 import gsap from "gsap";
+import { useAtom, useSetAtom } from "jotai";
 import * as React from "react";
 import * as THREE from "three";
-import useSound from "./use-sound";
+import { axisAtom, xAtom, yAtom } from "../atoms";
+import SCREENS from "../screens";
 import useSceneHelpers from "./use-scene-helpers";
+import useSound from "./use-sound";
 
-export default function useSceneInteractions() {
+export default function useSceneInteractions(
+  sceneRef: React.RefObject<THREE.Group>
+) {
   const { playSound } = useSound();
 
   const { get3dObjectByName } = useSceneHelpers();
 
-  const handleButtonPress = (
-    sceneRef: React.RefObject<THREE.Group>,
-    buttonName: string
-  ) => {
-    const button = get3dObjectByName(sceneRef, buttonName);
+  const [x, setX] = useAtom(xAtom);
 
+  const [axis, setAxis] = useAtom(axisAtom);
+
+  const setY = useSetAtom(yAtom);
+
+  const handleButtonPress = (buttonName: string) => {
+    const button = get3dObjectByName(sceneRef, buttonName);
     if (button) {
       playSound(500);
       gsap.to(button.position, {
@@ -27,7 +34,54 @@ export default function useSceneInteractions() {
     }
   };
 
+  const pressOK = () => {
+    setAxis("VERTICAL");
+    handleButtonPress("A_button");
+  };
+
+  const pressBack = () => {
+    setAxis("HORIZONTAL");
+    handleButtonPress("B_button");
+  };
+
+  const goRight = () => {
+    setX((old) => {
+      if (old < SCREENS.length - 1) return old + 1;
+      return old;
+    });
+    handleButtonPress("RIGHT_button");
+  };
+
+  const goLeft = () => {
+    if (axis === "HORIZONTAL")
+      setX((old) => {
+        if (old > 0) return old - 1;
+        return old;
+      });
+    handleButtonPress("LEFT_button");
+  };
+
+  const goUp = () => {
+    if (axis === "VERTICAL")
+      setY((old) => {
+        if (old > 0) return old - 1;
+        return old;
+      });
+    handleButtonPress("UP_button");
+  };
+
+  const goDown = () => {
+    if (axis === "VERTICAL")
+      setY((old) => {
+        if (SCREENS[x].screens && old < SCREENS[x].screens.length - 1)
+          return old + 1;
+        return old;
+      });
+    handleButtonPress("DOWN_button");
+  };
+
   return {
-    handleButtonPress,
+    direction: { goUp, goDown, goLeft, goRight },
+    buttons: { pressOK, pressBack },
   };
 }
