4954be3c9a387172ee1bda893a6c120e8a649a0a
Hamza Ayech <4yech.hamza@gmail.com>
2025-02-24 14:40:21 +0100

    Logic implementation for buttons pressing handling

--- src/hooks/use-scene-interactions.ts
+++ src/hooks/use-scene-interactions.ts
@@ -1,11 +1,14 @@
 import gsap from "gsap";
-import { useAtom, useSetAtom } from "jotai";
+import { useAtom } from "jotai";
 import * as React from "react";
 import * as THREE from "three";
 import { axisAtom, xAtom, yAtom } from "../atoms";
 import SCREENS from "../screens";
+import { OngoingClick } from "../types";
 import useSceneHelpers from "./use-scene-helpers";
 import useSound from "./use-sound";
+
+const CLICK_ANIMATION_DURATION = 0.3;
 
 export default function useSceneInteractions(
   sceneRef: React.RefObject<THREE.Group>
@@ -14,74 +17,180 @@ export default function useSceneInteractions(
 
   const { get3dObjectByName } = useSceneHelpers();
 
+  const [ongoingClicks, setOngoingClicks] = React.useState<Array<OngoingClick>>(
+    []
+  );
+
   const [x, setX] = useAtom(xAtom);
 
   const [axis, setAxis] = useAtom(axisAtom);
 
-  const setY = useSetAtom(yAtom);
+  const [y, setY] = useAtom(yAtom);
 
-  const handleButtonPress = (buttonName: string) => {
-    const button = get3dObjectByName(sceneRef, buttonName);
-    if (button) {
-      playSound(500);
-      gsap.to(button.position, {
-        z: "-=0.025",
-        duration: 0.4,
-        ease: "power2.in",
-        yoyo: true,
-        repeat: 1,
-      });
-    }
-  };
+  const isClickOngoing = React.useCallback(
+    (click: OngoingClick) => ongoingClicks.includes(click),
+    [ongoingClicks]
+  );
 
-  const pressOK = () => {
-    setAxis("VERTICAL");
-    handleButtonPress("A_button");
-  };
+  const setAsOngoingClick = React.useCallback(
+    (click: OngoingClick) => {
+      setOngoingClicks([...ongoingClicks, click]);
+      setTimeout(() => {
+        setOngoingClicks(
+          ongoingClicks.filter((ongoingClick) => ongoingClick !== click)
+        );
+      }, CLICK_ANIMATION_DURATION * 1000);
+    },
+    [ongoingClicks]
+  );
 
-  const pressBack = () => {
-    setAxis("HORIZONTAL");
-    handleButtonPress("B_button");
-  };
+  const handleButtonPress = React.useCallback(
+    (
+      buttonName: string,
+      onComplete: VoidFunction,
+      shouldPlaySound: boolean
+    ) => {
+      const button = get3dObjectByName(sceneRef, buttonName);
+      if (button) {
+        const tl = gsap.timeline({
+          onComplete,
+          onStart: () => {
+            if (shouldPlaySound) playSound(500);
+          },
+        });
+        tl.to(button.position, {
+          z: "-=0.020",
+          duration: CLICK_ANIMATION_DURATION / 2,
+          ease: "power2.in",
+        }).to(button.position, {
+          z: "+=0.020",
+          duration: CLICK_ANIMATION_DURATION / 2,
+          ease: "power2.in",
+        });
+      }
+    },
+    [get3dObjectByName, playSound, sceneRef]
+  );
 
-  const goRight = () => {
-    setX((old) => {
-      if (old < SCREENS.length - 1) return old + 1;
-      return old;
-    });
-    handleButtonPress("RIGHT_button");
-  };
+  const pressOK = React.useCallback(() => {
+    if (isClickOngoing("A")) return;
+    setAsOngoingClick("A");
+    handleButtonPress(
+      "A_button",
+      () => setAxis("VERTICAL"),
+      axis === "HORIZONTAL"
+    );
+  }, [axis, handleButtonPress, isClickOngoing, setAsOngoingClick, setAxis]);
 
-  const goLeft = () => {
-    if (axis === "HORIZONTAL")
-      setX((old) => {
-        if (old > 0) return old - 1;
-        return old;
-      });
-    handleButtonPress("LEFT_button");
-  };
+  const pressBack = React.useCallback(() => {
+    if (isClickOngoing("B")) return;
+    setAsOngoingClick("B");
+    handleButtonPress(
+      "B_button",
+      () => {
+        setAxis("HORIZONTAL");
+        setY(0);
+      },
+      axis === "VERTICAL"
+    );
+  }, [
+    axis,
+    handleButtonPress,
+    isClickOngoing,
+    setAsOngoingClick,
+    setAxis,
+    setY,
+  ]);
 
-  const goUp = () => {
-    if (axis === "VERTICAL")
-      setY((old) => {
-        if (old > 0) return old - 1;
-        return old;
-      });
-    handleButtonPress("UP_button");
-  };
+  const pressHome = React.useCallback(() => {
+    if (isClickOngoing("Y")) return;
+    setAsOngoingClick("Y");
+    handleButtonPress(
+      "Y_button",
+      () => {
+        setAxis("HORIZONTAL");
+        setY(0);
+        setX(0);
+      },
+      axis === "VERTICAL"
+    );
+  }, [
+    axis,
+    handleButtonPress,
+    isClickOngoing,
+    setAsOngoingClick,
+    setAxis,
+    setX,
+    setY,
+  ]);
 
-  const goDown = () => {
-    if (axis === "VERTICAL")
-      setY((old) => {
-        if (SCREENS[x].screens && old < SCREENS[x].screens.length - 1)
-          return old + 1;
-        return old;
-      });
-    handleButtonPress("DOWN_button");
-  };
+  const goRight = React.useCallback(() => {
+    if (isClickOngoing("RIGHT")) return;
+    setAsOngoingClick("RIGHT");
+    handleButtonPress(
+      "RIGHT_button",
+      () => {
+        if (axis === "VERTICAL") return;
+        setX((old) => {
+          if (old < SCREENS.length - 1) return old + 1;
+          return old;
+        });
+      },
+      axis === "HORIZONTAL" && x < SCREENS.length - 1
+    );
+  }, [axis, handleButtonPress, isClickOngoing, setAsOngoingClick, setX, x]);
+
+  const goLeft = React.useCallback(() => {
+    if (isClickOngoing("LEFT")) return;
+    setAsOngoingClick("LEFT");
+    handleButtonPress(
+      "LEFT_button",
+      () => {
+        if (axis === "VERTICAL") return;
+        setX((old) => {
+          if (old > 0) return old - 1;
+          return old;
+        });
+      },
+      axis === "HORIZONTAL" && x > 0
+    );
+  }, [axis, handleButtonPress, isClickOngoing, setAsOngoingClick, setX, x]);
+
+  const goUp = React.useCallback(() => {
+    if (isClickOngoing("UP")) return;
+    setAsOngoingClick("UP");
+    handleButtonPress(
+      "UP_button",
+      () => {
+        if (axis === "HORIZONTAL") return;
+        setY((old) => {
+          if (old > 0) return old - 1;
+          return old;
+        });
+      },
+      axis === "VERTICAL" && y > 0
+    );
+  }, [axis, handleButtonPress, isClickOngoing, setAsOngoingClick, setY, y]);
+
+  const goDown = React.useCallback(() => {
+    if (isClickOngoing("DOWN")) return;
+    setAsOngoingClick("DOWN");
+    handleButtonPress(
+      "DOWN_button",
+      () => {
+        if (axis === "HORIZONTAL") return;
+        setY((old) => {
+          if (SCREENS[x].screens && old < SCREENS[x].screens.length - 1)
+            return old + 1;
+          return old;
+        });
+      },
+      axis === "VERTICAL" && y < SCREENS[x].screens.length - 1
+    );
+  }, [axis, handleButtonPress, isClickOngoing, setAsOngoingClick, setY, x, y]);
 
   return {
     direction: { goUp, goDown, goLeft, goRight },
-    buttons: { pressOK, pressBack },
+    buttons: { pressOK, pressBack, pressHome },
   };
 }
